14ec51c24525f4dcf746dfada2e253d2199de9cb
hung222 <90371118+hung222@users.noreply.github.com>
2022-02-07 22:35:15 +0700

    Update app.py

--- tlflask/app.py
+++ tlflask/app.py
@@ -1,6 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route("tnflask/")
-def tnflas():
-    return "tnflask.html"
+
+from flask import Flask
+app = Flask("my website")
+
+ @app.route("/")
+ def giachoi():
+   return "giachoi"
+
+ @app.route("/giachoi/<string:name>")
+ def giachoi(name):
+    return "giachoi {}".format(name)
+
+@app.route("app.py/")
+ def app.py():
+    return "app.py"
+
+
+
+ 
+
